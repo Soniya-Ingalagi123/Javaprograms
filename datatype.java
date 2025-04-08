b4a7d736f49491d3c776f98898e9db2d2e537b56
sukanya0512 <sukanyamulimani05@gmail.com>
2025-04-09 04:10:51 +0530

    save

---
+++ datatype.java
@@ -0,0 +1,17 @@
+package Javaprograms;
+class datatype {
+      public static void main(String[] args) {
+            char a = 'b';
+            int i = 10;
+            byte b = 2;
+            short s = 56;
+            double d = 4.333d;
+            float f = 1.3f;
+            System.out.println("Char:" + a);
+            System.out.println("Integer:" + i);
+            System.out.println("Byte:" + b);
+            System.out.println("Shorts" + s);
+            System.out.println("Double :" + d);
+            System.out.println("Float:" + f);
+      }
+}
